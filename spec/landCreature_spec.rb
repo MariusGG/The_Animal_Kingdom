f4e7b9c5744e4fbeb85cbbe109a4929105ac2e8d
mariusgg <marius.g.guei@gmail.com>
2019-01-16 00:18:59 +0000

    failing random number generator

--- spec/landCreature_spec.rb
+++ spec/landCreature_spec.rb
@@ -6,5 +6,11 @@ describe LandCreature do
 
    it "should should have a starting health point of 10" do
      expect(subject.hp).to eq 10
- end
+   end
+
+   it "has a random amount generator" do
+     expect(subject).to receive(:rand).and_return(12345)
+     expect(subject.amount).to eq 12345
+   end
+
 end
