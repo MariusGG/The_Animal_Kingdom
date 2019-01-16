d5e935776e5764ef1190eb6e843413547ac33704
mariusgg <marius.g.guei@gmail.com>
2019-01-16 00:06:26 +0000

    passing landCreatures hp test

--- spec/landCreature_spec.rb
+++ spec/landCreature_spec.rb
@@ -3,4 +3,8 @@ require 'landCreature'
 describe LandCreature do
 
    it { expect(described_class).to be < TheKingdom }
+
+   it "should should have a starting health point of 10" do
+     expect(subject.hp).to eq 10
+ end
 end
